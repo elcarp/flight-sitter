0d4a90b31d54319f940fb0a6a40528c34f45a278
elcarp <lise.carpenter@gmail.com>
2025-04-15 11:02:13 +0700

    clean up

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,7 +4,7 @@ import { Button } from './ui/button'
 
 export default function Nav() {
   return (
-    <header className='w-full bg-white shadow-md p-4 flex items-center justify-between absolute top-0 z-50'>
+    <header className='w-full bg-white shadow-md p-4 flex items-center justify-between absolutetop-0 z-50'>
       <Link href='/'>
         <h1 className='text-2xl font-bold text-gray-800 flex items-center gap-2'>
           <FaPlaneDeparture /> Flight Sitter <FaHeart />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import { Button } from '~components/ui/button'
-import { Card, CardContent, CardHeader, CardTitle } from '~components/ui/card'
-import Link from 'next/link'
 import Hero from '~components/hero'
 
 export default function Home() {
@@ -9,50 +6,6 @@ export default function Home() {
     <div className='min-h-screen bg-gray-100 flex flex-col items-center'>
       <main className='w-full'>
         <Hero />
-        <h2 className='text-3xl font-bold text-gray-900'>
-          Find a Flight Companion
-        </h2>
-        <p className='text-gray-600 mt-2'>
-          Whether you&apos;re a parent traveling with kids or need assistance
-          during your flight, find a reliable companion to make your journey
-          easier.
-        </p>
-
-        <div className='mt-6 grid grid-cols-1 md:grid-cols-2 gap-6'>
-          <Card>
-            <CardHeader>
-              <CardTitle>Need a Flight Sitter?</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className='text-gray-700'>
-                Find a trusted travel companion to assist you or your child
-                during your flight.
-              </p>
-              <Link href='/find-a-sitter'>
-                <Button className='mt-4 w-full cursor-pointer'>
-                  Find a Sitter
-                </Button>
-              </Link>
-            </CardContent>
-          </Card>
-
-          <Card>
-            <CardHeader>
-              <CardTitle>Become a Flight Sitter</CardTitle>
-            </CardHeader>
-            <CardContent>
-              <p className='text-gray-700'>
-                Earn rewards by helping fellow travelers during their flight
-                journey.
-              </p>
-              <Link href='/sitter-signup'>
-                <Button className='mt-4 w-full cursor-pointer'>
-                  Sign Up as a Sitter
-                </Button>
-              </Link>
-            </CardContent>
-          </Card>
-        </div>
       </main>
     </div>
   )
